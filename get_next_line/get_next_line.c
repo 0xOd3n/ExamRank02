d7cf3e9c732eff5cc35137b498afe712844457bc
Teresa bulungu Lufuluabo pastor <tlufulua@student.42madrid.com>
2021-09-30 14:12:29 +0200

    Working

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,12 +6,26 @@
 /*   By: tlufulua <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 22:50:22 by tlufulua          #+#    #+#             */
-/*   Updated: 2021/09/15 14:57:45 by tlufulua         ###   ########.fr       */
+/*   Updated: 2021/09/30 14:11:03 by tlufulua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "libft.h"
+
+char	*ft_strnjoin(char *s1, char *s2, int n)
+{
+	char	*aux;
+	int		i;
+
+	i = 0;
+	aux = (char *)malloc(sizeof(char) * (n + ft_strlen(s1)));
+	while (*s1)
+		aux[i++] = *s1++;
+	while (n--)
+		aux[i++] = *s2++;
+	return (aux);
+}
 
 char	*get_next_line(int fd)
 {
@@ -19,7 +33,9 @@ char	*get_next_line(int fd)
 	int		rd;
 	char	buffer[BUFFER_SIZE + 1];
 	char	*aux;
+	int		i;
 
+	i = 0;
 	aux = 0;
 	rd = 1;
 	line = ft_strdup("");
@@ -29,9 +45,12 @@ char	*get_next_line(int fd)
 		buffer[rd] = '\0';
 		if (rd > 0)
 		{
+			while (*buffer && *buffer != '\n')
+				i++;
 			aux = line;
-			line = ft_strjoin(aux, buffer);
+			line = ft_strnjoin(aux, buffer, i);
 			free(aux);
+		//FALTA ITERAR BUFFER HASTA DESPUÉS DE LO COPIADO EN LINE
 		}
 	}
 	return (line);
--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,7 +6,7 @@
 /*   By: tlufulua <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 00:20:50 by tlufulua          #+#    #+#             */
-/*   Updated: 2021/09/15 14:59:08 by tlufulua         ###   ########.fr       */
+/*   Updated: 2021/09/30 14:11:10 by tlufulua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	main()
 	int		fd;
 
 	fd = 0;
-	fd = open("./text.txt", O_RDONLY);
+//	fd = open("./text.txt", O_RDONLY);
 	line = get_next_line(fd);
 	printf("\nline: %s\n", line);
 	if (line && *line)
